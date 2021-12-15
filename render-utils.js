4bfddc5ae2dd5e3c1b5cab59cbe0edc0bd612f2f
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-14 17:33:26 -0800

    wrote renderCakeDetail function

--- render-utils.js
+++ render-utils.js
@@ -17,5 +17,25 @@ export function renderCakeCard(cake) {
 export function renderCakeDetail(cake) {
     const div = document.createElement('div');
     const img = document.createElement('img');
+    const nameEl = document.createElement('p');
+    const flavorEl = document.createElement('p');
+    const icingEl = document.createElement('p');
+    const decorationEl = document.createElement('p');
+    
+    div.classList.add('cake-detail');
 
+    nameEl.textContent = cake.name;
+    nameEl.classList.add('cake-name');
+
+    flavorEl.textContent = `flavor: ${cake.flavor}`;
+
+    icingEl.textContent = `icing: ${cake.icing}`;
+
+    decorationEl.textContent = `decoration: ${cake.icing}`;
+
+    img.src = `../assets/${cake.name}.jpeg`;
+
+    div.append(img, nameEl, flavorEl, icingEl, decorationEl);
+    
+    return div;
 }
